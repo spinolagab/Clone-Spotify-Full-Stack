96cc7cd69e68e1959de947ed21c38b4e2717f2ac
Gabriel Spinola <spinolagab@gmail.com>
2025-03-02 22:37:27 -0300

    Busca binaria implementada para ID hexadecimal + pagina de busca indevida

--- front-end/src/pages/Artist.jsx
+++ front-end/src/pages/Artist.jsx
@@ -9,65 +9,76 @@ import { songsArray } from "../assets/database/songs";
 const Artist = () => {
   // id da pagina salvo
   const { id } = useParams();
-  // Se os ids fossem ordenados poderia usar busca binaria
-  // mas como usamos os ids robustos do mongo DB sera uma busca em tempo O(n)
 
-  // busca binaria usada anteriormente antes de utilizar o banco de dados + backend
-  // let initialPos = 0;
-  // let finalPos = artistArray.length - 1;
-  // let middlePos = Math.floor((initialPos + finalPos) / 2);
-  // if (Number(id) <= finalPos && Number(id) >= initialPos) {
-  //   // Busca binária pelo artista com o id desejado
-  //   while (artistArray[middlePos].id !== Number(id)) {
-  //     if (artistArray[middlePos].id > id) {
-  //       finalPos = middlePos - 1;
-  //     } else {
-  //       initialPos = middlePos + 1;
-  //     }
-  //     middlePos = Math.floor((initialPos + finalPos) / 2);
-  //   }
+  // busca binaria atualizada para o modelo de ID hexadecimal (já ordenado) O(log n)
+  let initialPos = 0;
+  let finalPos = artistArray.length - 1;
+  let middlePos = Math.floor((initialPos + finalPos) / 2);
+  if (
+    id.localeCompare(artistArray[finalPos]._id) <= 0 &&
+    id.localeCompare(artistArray[0]._id) >= 0
+  ) {
+    // Busca binária pelo artista com o id desejado
+    while (id.localeCompare(artistArray[middlePos]._id) != 0) {
+      let compare = id.localeCompare(artistArray[middlePos]._id);
+      if (compare < 0) {
+        finalPos = middlePos - 1;
+      } else {
+        initialPos = middlePos + 1;
+      }
+      middlePos = Math.floor((initialPos + finalPos) / 2);
+    }
 
-  //   const { name, banner } = artistArray[middlePos];
+    const { name, banner } = artistArray[middlePos];
 
-  // formato de busca atual para ser compativel
-  const { name, banner } = artistArray.filter(
-    (currentArtistObj) => currentArtistObj._id === id
-  )[0];
+    // formato de busca linear usado anteriormente O(n)
+    // const { name, banner } = artistArray.filter(
+    //   (currentArtistObj) => currentArtistObj._id === id
+    // )[0];
 
-  // Lista de músicas do artista encontrado
-  const songsArrayFromArtist = songsArray.filter(
-    (currentSongObj) => currentSongObj.artist === name
-  );
+    // Lista de músicas do artista encontrado
+    const songsArrayFromArtist = songsArray.filter(
+      (currentSongObj) => currentSongObj.artist === name
+    );
 
-  const randomIndex = Math.floor(
-    Math.random() * (songsArrayFromArtist.length - 1)
-  );
-  const randomSongFromArtist = songsArrayFromArtist[randomIndex]._id;
+    const randomIndex = Math.floor(
+      Math.random() * (songsArrayFromArtist.length - 1)
+    );
+    const randomSongFromArtist = songsArrayFromArtist[randomIndex]._id;
 
-  return (
-    <div className="artist">
-      <div
-        className="artist__header"
-        style={{
-          backgroundImage: `linear-gradient(to bottom, var(--_shade), var(--_shade)), url(${banner}`,
-        }}
-      >
-        <h2 className="artist__title">{name}</h2>
+    return (
+      <div className="artist">
+        <div
+          className="artist__header"
+          style={{
+            backgroundImage: `linear-gradient(to bottom, var(--_shade), var(--_shade)), url(${banner}`,
+          }}
+        >
+          <h2 className="artist__title">{name}</h2>
+        </div>
+        <div className="artist__body">
+          <h2>Populares</h2>
+
+          <SongList songsArray={songsArrayFromArtist} />
+        </div>
+        <Link to={`/song/${randomSongFromArtist}`}>
+          <FontAwesomeIcon
+            // class name composta para pegar o css de ambos os single-item especificados
+            className="single-item__icon  single-item__icon--artist"
+            icon={faCirclePlay}
+          />
+        </Link>
       </div>
-      <div className="artist__body">
-        <h2>Populares</h2>
-
-        <SongList songsArray={songsArrayFromArtist} />
+    );
+  } else {
+    // Acesso indevido
+    return (
+      <div>
+        <h1>Page not found!</h1>
+        <h2>Index out of range</h2>
       </div>
-      <Link to={`/song/${randomSongFromArtist}`}>
-        <FontAwesomeIcon
-          // class name composta para pegar o css de ambos os single-item especificados
-          className="single-item__icon  single-item__icon--artist"
-          icon={faCirclePlay}
-        />
-      </Link>
-    </div>
-  );
+    );
+  }
 };
 
 export default Artist;
--- front-end/src/pages/Song.jsx
+++ front-end/src/pages/Song.jsx
@@ -8,85 +8,96 @@ const Song = () => {
   // também pode ser const songId = useParams().id;
   const { id } = useParams();
 
-  // Se os ids fossem ordenados poderia usar busca binaria
-  // mas como usamos os ids robustos do mongo DB sera uma busca em tempo O(n)
-
-  // Busca binária feita anteriormente para encontrar a música desejada
-  // let initialPos = 0;
-  // let finalPos = songsArray.length - 1;
-  // let middlePos = Math.floor((initialPos + finalPos) / 2);
+  // busca binaria atualizada para o modelo de ID hexadecimal (já ordenado) O(log n)
+  let initialPos = 0;
+  let finalPos = songsArray.length - 1;
+  let middlePos = Math.floor((initialPos + finalPos) / 2);
   // // Validação para entradas indevidas
-  // if (Number(id) <= finalPos && Number(id) >= initialPos) {
-  //   // Busca binária pelo artista com o id desejado
-  //   while (songsArray[middlePos].id !== Number(id)) {
-  //     if (songsArray[middlePos].id > id) {
-  //       finalPos = middlePos - 1;
-  //     } else {
-  //       initialPos = middlePos + 1;
-  //     }
-  //     middlePos = Math.floor((initialPos + finalPos) / 2);
-  //   }
+  if (
+    id.localeCompare(songsArray[finalPos]._id) <= 0 &&
+    id.localeCompare(songsArray[0]._id) >= 0
+  ) {
+    // Busca binária pela musica com o id desejado
+    while (id.localeCompare(songsArray[middlePos]._id) != 0) {
+      let compare = id.localeCompare(songsArray[middlePos]._id);
+      if (compare < 0) {
+        finalPos = middlePos - 1;
+      } else {
+        initialPos = middlePos + 1;
+      }
+      middlePos = Math.floor((initialPos + finalPos) / 2);
+    }
 
-  //   // Se id já não estivesse sendo usado seria possível desestruturar ele da variável
-  //   const { image, name, duration, artist, audio } = songsArray[middlePos];
-  const { image, name, duration, artist, audio } = songsArray.filter(
-    (currentSongObj) => currentSongObj._id === id
-  )[0];
+    const { image, name, duration, artist, audio } = songsArray[middlePos];
 
-  // Busca linear pelo nome do artista (nomes não estão ordenados)
-  const artistObj = artistArray.filter(
-    (currentArtistObj) => currentArtistObj.name === artist
-  )[0];
+    // Metodo de busca usado anteriormente em O(n)
+    // const { image, name, duration, artist, audio } = songsArray.filter(
+    //   (currentSongObj) => currentSongObj._id === id
+    // )[0];
 
-  const songsArrayFromArtist = songsArray.filter(
-    (currentSongObj) => currentSongObj.artist === artist
-  );
+    // Busca linear pelo nome do artista (nomes não estão ordenados)
+    const artistObj = artistArray.filter(
+      (currentArtistObj) => currentArtistObj.name === artist
+    )[0];
 
-  // Funciona como se o shuffle estivesse sempre ativo e vai para músicas aleatórias
-  const randomIndex = Math.floor(
-    Math.random() * (songsArrayFromArtist.length - 1)
-  );
-  const randomSongFromArtist = songsArrayFromArtist[randomIndex]._id;
+    const songsArrayFromArtist = songsArray.filter(
+      (currentSongObj) => currentSongObj.artist === artist
+    );
 
-  const randomIndex2 = Math.floor(
-    Math.random() * (songsArrayFromArtist.length - 1)
-  );
-  const randomSongFromArtist2 = songsArrayFromArtist[randomIndex2]._id;
+    // Funciona como se o shuffle estivesse sempre ativo e vai para músicas aleatórias
+    const randomIndex = Math.floor(
+      Math.random() * (songsArrayFromArtist.length - 1)
+    );
+    const randomSongFromArtist = songsArrayFromArtist[randomIndex]._id;
 
-  return (
-    <div className="song">
-      <div className="song__container">
-        <div className="song__image-container">
-          <img src={image} alt={`Song ${name} Image`} />
-        </div>
-      </div>
+    const randomIndex2 = Math.floor(
+      Math.random() * (songsArrayFromArtist.length - 1)
+    );
+    const randomSongFromArtist2 = songsArrayFromArtist[randomIndex2]._id;
 
-      <div className="song__bar">
-        <Link to={`/artist/${artistObj._id}`}>
-          <div className="song__artist-image">
-            <img
-              width={75}
-              height={75}
-              src={artistObj.image}
-              alt={`${artist} Artist Image`}
-            />
+    return (
+      <div className="song">
+        <div className="song__container">
+          <div className="song__image-container">
+            <img src={image} alt={`Song ${name} Image`} />
           </div>
-        </Link>
+        </div>
 
-        <Player
-          duration={duration}
-          randomSongFromArtist={randomSongFromArtist}
-          randomSongFromArtist2={randomSongFromArtist2}
-          audio = {audio}
-        />
+        <div className="song__bar">
+          <Link to={`/artist/${artistObj._id}`}>
+            <div className="song__artist-image">
+              <img
+                width={75}
+                height={75}
+                src={artistObj.image}
+                alt={`${artist} Artist Image`}
+              />
+            </div>
+          </Link>
 
-        <div>
-          <p className="song__name">{name}</p>
-          <p>{artist}</p>
+          <Player
+            duration={duration}
+            randomSongFromArtist={randomSongFromArtist}
+            randomSongFromArtist2={randomSongFromArtist2}
+            audio={audio}
+          />
+
+          <div>
+            <p className="song__name">{name}</p>
+            <p>{artist}</p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    // pagina de busca indevida
+    return (
+      <div>
+        <h1>Page not found!</h1>
+        <h2>Song doesn't exist or has yet to release.</h2>
+      </div>
+    );
+  }
 };
 
 export default Song;
